b9475dc4a748248800a28590205b0d94d65b5b79
vergto <denis27012000@gmail.com>
2019-09-21 17:20:28 +0300

    first commit

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot('952476420:AAHOxzyLhPslDRyRMaxGY2YTZN-ZlGrpwIU')
 def start_message(message):
     async def process_start_command(message: types.Message):
         await message.reply("Привет!", reply_markup=kb.greet_kb)
- 
+
    # bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
 bot.polling()
