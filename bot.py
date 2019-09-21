9db6382d41899b7285354d4d77bb6f84a22e08cc
vergto <denis27012000@gmail.com>
2019-09-21 17:38:18 +0300

    first commit

--- bot.py
+++ bot.py
@@ -10,13 +10,5 @@ bot = telebot.TeleBot('952476420:AAHOxzyLhPslDRyRMaxGY2YTZN-ZlGrpwIU')
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: bot.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
-
-
-@dp.message_handler()
-async def echo_message(msg: bot.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
 
 bot.polling()
