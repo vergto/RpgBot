41a756a9769f13fea86120e1dad4a7de55036d76
vergto <denis27012000@gmail.com>
2019-09-21 22:08:45 +0300

    first commit

--- bot.py
+++ bot.py
@@ -6,15 +6,19 @@ import sqlite3
 bot = telebot.TeleBot('952476420:AAHOxzyLhPslDRyRMaxGY2YTZN-ZlGrpwIU')
 
 
-# bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 @bot.message_handler(commands=['start'])
 def start_message(message):
     start = telebot.types.ReplyKeyboardMarkup(True, False)
-    start.row('Бой')
-    start.row('Профиль')
-    start.row('Инвентарь')
-    start.row('Войти')
-    bot.send_message(message.from_user.id, 'Выбери действие', reply_markup=start)
+    itembtna = telebot.types.KeyboardButton('Бой')
+    itembtnb = telebot.types.KeyboardButton('Профиль')
+    start.row(itembtna, itembtnb)
+    bot.send_message(message.from_user.id, "Choose one letter:", reply_markup=start)
+
+    # start.row('Бой')
+    # start.row('Профиль')
+    # start.row('Инвентарь')
+    # start.row('Войти')
+    # bot.send_message(message.from_user.id, 'Выбери действие', reply_markup=start)
 
 bot.polling()
 
