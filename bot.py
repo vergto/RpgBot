2b2daf16f8301f51efe69d38276817b74a0a5ff9
vergto <denis27012000@gmail.com>
2019-09-22 18:53:40 +0300

    first commit

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ users = sqlite3.connect("users.db")
 with users:
     cur = users.cursor()
     cur.execute("DROP TABLE IF EXISTS Users")
-    cur.execute("CREATE TABLE Users(Id INT, UserName TEXT, Name TEXT, Strength, Agility, Stamina, Luck)")
+    cur.execute("CREATE TABLE Users(Id INT, UserName TEXT, Name TEXT, Strength, intellect, Agility, Stamina, Luck)")
 cur.close()
 
 
@@ -52,8 +52,8 @@ def hello(message):
     name = message.from_user.first_name
     with users:
         cur = users.cursor()
-        cur.execute("""INSERT INTO Users VALUES(?,?,?,?,?,?,?);""",
-                    (str(message.from_user.id), str(name), str(message.text), str('5'), str('5'), str('5'), str('5')))
+        cur.execute("""INSERT INTO Users VALUES(?,?,?,?,?,?,?,?);""",
+                    (str(message.from_user.id), str(name), str(message.text), str('5'), str('5'), str('5'), str('5'), str('5')))
     cur.close()
 
 @bot.message_handler(content_types=['text'])
