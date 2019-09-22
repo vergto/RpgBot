5dbcc4b351af0d6df2342e5b9a8b62d5472e6413
vergto <denis27012000@gmail.com>
2019-09-22 18:39:01 +0300

    first commit

--- bot.py
+++ bot.py
@@ -77,7 +77,7 @@ def get_text_messages(message):
     elif (message.text == "Пользователи" or message.text == "пользователи"):
         bot.callback_query_handler(users_list(message))
 
-    # start.row('Бой')  #аама
+    # start.row('Бой')  #аамаааааа
     # start.row('Профиль')
     # start.row('Инвентарь')
     # start.row('Войти')
