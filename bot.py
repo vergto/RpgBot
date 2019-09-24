da666138e3838fe16f912aa5b8ae5da11782d2b1
vergto <denis27012000@gmail.com>
2019-09-24 14:00:02 +0300

    first commit

--- bot.py
+++ bot.py
@@ -199,8 +199,8 @@ def fight_battle_monster(type_monster_battle,message):
         flagg = 1
     else:
         flagg = 0
-    bot.send_message(message.from_user.id, str(rows[0][1])+ ": " +str(hero_hp) + "❤ / " + str(type_monster_battle)
-                     + ": " + str(monster_hp) + "❤")
+    fight_logs_battle = str(rows[0][1])+ ": " +str(hero_hp) + "❤ / " + str(type_monster_battle)+ ": " + str(monster_hp) + "❤"
+    bot.send_message(message.from_user.id, fight_logs_battle)
     while monster_hp >= 1 and hero_hp >= 1:
         hero_dmg = round(rows[0][12] * random.random() * 4)
         monster_dmg = round(50 + monster_lvl * random.random() * 4)
