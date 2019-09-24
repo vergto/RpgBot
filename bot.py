62ebf5082740f7bb79c087ce925a618ee777120a
vergto <denis27012000@gmail.com>
2019-09-25 01:46:37 +0300

    first commit

--- bot.py
+++ bot.py
@@ -193,7 +193,6 @@ def rand_gold_battle(fight_logs_battle, monster_lvl, message):
         rows = cur.fetchall()
         gold_plus = round(monster_lvl * random.randint(1, 15))
         bot.send_message(message.from_user.id, str(gold_plus))
-      # cur.execute("UPDATE Users SET HP = HP+20 WHERE  Id=" + str(message.from_user.id))
         bot.send_message(message.from_user.id, str(gold_plus))
         fight_logs_battle = fight_logs_battle + "\nполучено: " + str(rows[0][7]) + "+" + str(gold_plus) + " золота💰"
         bot.send_message(message.from_user.id, fight_logs_battle)
@@ -226,7 +225,8 @@ def lvl_up_hero(fight_logs_battle, monster_lvl, message):
         if lvl_up_flag == 0:
             fight_logs_battle = fight_logs_battle + "\nВаш уровень: " + str(rows[0][8]) + "   " \
                                 + str(rows[0][9] + experience_lvl) + "/" + str(rows[0][10])
-        cur.execute("UPDATE Users SET HP = HP+" + str(rand_gold_battle(fight_logs_battle, monster_lvl, message)) + " WHERE  Id=" + str(message.from_user.id))
+        cur.execute("UPDATE Users SET Gold = Golg+" + str(
+            rand_gold_battle(fight_logs_battle, monster_lvl, message)) + " WHERE  Id=" + str(message.from_user.id))
     cur.close()
 
 
