55bfa6e65f0de7ebcbd51c412021cf04d50d9779
vergto <denis27012000@gmail.com>
2019-10-09 02:27:59 +0300

    first commit

--- bmenu.py
+++ bmenu.py
@@ -3,6 +3,7 @@ import sqlite3
 bot = telebot.TeleBot('952476420:AAHOxzyLhPslDRyRMaxGY2YTZN-ZlGrpwIU')
 
 
+# Кнопка назад, возвращает стартовый интерфейс
 def rearwards(message):
     rearw = telebot.types.ReplyKeyboardMarkup(True, False)
     itembtna = telebot.types.KeyboardButton('Бой ⚔')
@@ -15,6 +16,7 @@ def rearwards(message):
     bot.send_message(message.from_user.id, "Вы вернулись в стартовое меню", reply_markup=rearw)
 
 
+# Окно пользователя, показывает характеристики персонажа
 def users_window(message):
     users = sqlite3.connect("users.db")
     with users:
@@ -28,6 +30,7 @@ def users_window(message):
         bot.send_message(message.from_user.id, "Профиль игрока: " + str(rows[0][1]) + "\n" \
                                                 "Уровень: " + str(rows[0][8]) + "   " \
                                                 + str(rows[0][9]) + "/" + str(rows[0][10]) + "\n\n" \
+                                                "🗺 Карта: /map \n\n" \
                                                 "❤ HP: " + str(rows[0][11]) + "\n" \
                                                 "🔪 DMG: " + str(rows[0][12]) + "\n\n" \
                                                 "💪 Сила: " + str(rows[0][2]) + "\n" \
