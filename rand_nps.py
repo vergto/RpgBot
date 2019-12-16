6721870982e59ff4a7f6826ecdb3b02b7f77a7d5
vergto <denis27012000@gmail.com>
2019-12-16 12:33:26 +0300

    first commit

--- rand_nps.py
+++ rand_nps.py
@@ -3,33 +3,34 @@ import random
 
 class Rand_monster(object):
 
-    def __init__(self, A):
-        if A == 0:
+    def __init__(self, numrand):
+        self.numrand = numrand
+        if numrand == 0:
             monter = ["Гоблин", "Слизень", "Крыс"]
             self.rand_monster = random.choice(monter)
-        elif A == 1:
+        elif numrand == 1:
             monter = ["Паук", "Гоблин", "Слизень", "Крыс", "Зараженный", "Зомби"]
             self.rand_monster = random.choice(monter)
-        elif A == 2:
+        elif numrand == 2:
             monter = ["Песчанный Паук", "Песчанный Гоблин", "Песчанный Слизень", "Песчанный Крыс", "Песчанный Зомби"]
             self.rand_monster = random.choice(monter)
-        elif A == 3:
+        elif numrand == 3:
             monter = ["Водный Паук", "Водный Гоблин", "Водный Слизень", "Водный Крыс", "Водный Зараженный",
                       "Водный Зомби"]
             self.rand_monster = random.choice(monter)
-        elif A == 4:
+        elif numrand == 4:
             monter = ["Огненный Паук", "Огненный Гоблин", "Огненный Слизень", "Огненный Голем"]
             self.rand_monster = random.choice(monter)
-        elif A == 5:
+        elif numrand == 5:
             monter = ["Ведьма", "Умертвие", "Скелет", "Мимик"]
             self.rand_monster = random.choice(monter)
-        elif A == 6:
+        elif numrand == 6:
             monter = ["Рыба людоед", "Гоблин", "Сирена"]
             self.rand_monster = random.choice(monter)
-        elif A == 7:
+        elif numrand == 7:
             monter = ["Кракен"]
             self.rand_monster = random.choice(monter)
-        elif A == 8:
+        elif numrand == 8:
             monter = ["Дракон"]
             self.rand_monster = random.choice(monter)
 
